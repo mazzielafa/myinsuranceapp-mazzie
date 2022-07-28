2b1a2a83fcc5a7ef5be35792613cdfefc6c0b899
Mazzie <mazzieola@hotmail.com>
2022-07-28 12:54:40 +0200

    aks cluster

--- terraform/akscluster.tf
+++ terraform/akscluster.tf
@@ -1,11 +1,11 @@
-resource "akshackathonmazzie" "prefix" {} 
+resource "random_pet" "prefix" {}
 
 provider "azurerm" {
   features {}
 }
 
 resource "azurerm_resource_group" "default" {
-  name     = "${akshackathonmazzie.prefix.id}-rg"
+  name     = "${random_pet.prefix.id}-rg"
   location = "eastasia"
 
   tags = {
@@ -14,20 +14,20 @@ resource "azurerm_resource_group" "default" {
 }
 
 resource "azurerm_kubernetes_cluster" "default" {
-  name                = "${akshackathonmazzie.prefix.id}-aks"
+  name                = "${random_pet.prefix.id}-aks"
   location            = azurerm_resource_group.default.location
   resource_group_name = azurerm_resource_group.default.name
-  dns_prefix          = "${akshackathonmazzie.prefix.id}-k8s"
+  dns_prefix          = "${random_pet.prefix.id}-k8s"
 
   default_node_pool {
     name            = "default"
-    node_count      = 2
+    node_count      = 1
     vm_size         = "Standard_D2_v2"
     os_disk_size_gb = 30
   }
 
   service_principal {
-    client_id = var.APPID
+    client_id     = var.APPID
     client_secret = var.PASSWORD
   }
 
